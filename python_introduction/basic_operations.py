fd05c0c365efb0eb3c54a33e22bd935478d4974a
ericmuni-23 <munieric44@gmail.com>
2024-06-24 21:49:06 +0300

    this is a simple code showing adding substration and multiplacation

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+#!/usr/env python 3
+
+number1 = 10
+number2 = 5
+
+#Add number1 and number 2
+add = number1 + number2
+print(f'Addition of {number1} and {number2} is {add}')
+
+#substract number2 from number1
+diff = number1 - number2
+print(f"subtraction of {number1} and {number2} is {diff}")
+
+#Perform Multiplacation
+multiply = number1 * number2
+print(f"Multiplaction of {number1} and {number2} is {multiply}")
