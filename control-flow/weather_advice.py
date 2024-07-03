bc553b1acde0ac4b4be57c3e5e5401a4b5a35b9f
ericmuni-23 <munieric44@gmail.com>
2024-07-04 00:53:17 +0300

    A script asking a user about the current weather conditions and provide clothing recommendations

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -7,11 +7,11 @@ weather = input("What's the weather like today? (sunny/rainy/cold): " )
 
 # provide clothing recommendations based on weather input
 
-if weather == "sunny":
+if weather == ("sunny"):
     print("Recommendation: Wear a t-shirt and sun glasses.")
-elif weather == "rainy":
+elif weather == ("rainy"):
     print("Recommendation: Dont forget your umbrella and a raincoat.")
-elif weather == "cold":
+elif weather == ("cold"):
     print("Recommendation: Wear a warm coat and a scarf.")
 else:
     print("Sorry i dont have recomendations for ths weather.")
