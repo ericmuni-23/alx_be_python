3f8b959ff2d6fdab3bb508e95604b6a76cf37248
ericmuni-23 <munieric44@gmail.com>
2024-07-07 21:19:53 +0300

    Change the print statement

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -4,12 +4,12 @@
 weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
 # Provide clothing recommendations based on weather input
-if weather == (f"sunny"):
-     print("Recommendation: Wear a t-shirt and sunglasses.")
-elif weather == (f"rainy"):
-     print("Recommendation: Don't forget your umbrella and a raincoat.")
-elif weather == (f"cold"):
-     print("Recommendation: Make sure to wear a warm coat and a scarf.")
+if weather == "sunny":
+     print("Wear a t-shirt and sunglasses.")
+elif weather == "rainy":
+     print("Don't forget your umbrella and a raincoat.")
+elif weather == "cold":
+     print("Make sure to wear a warm coat and a scarf.")
 else:
      print("Sorry, I don't have recommendations for this weather.")
     
