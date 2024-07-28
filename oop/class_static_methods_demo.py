3a57f1b16b9062509449af484a9243d04605c94e
ericmuni-23 <munieric44@gmail.com>
2024-07-28 23:38:48 +0300

    A simple code to perform calculations.

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+class Calculator:
+    calculation_type = "Arithmetic Operations"
+
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @classmethod
+    def multiply(cls, a, b):
+        print(f"Calculation Type: {cls.calculation_type}")
+        return a * b
+
+
+# Example usage:
+print(Calculator.add(6, 7))  # Output: 113
+print(Calculator.multiply(5, 6))  # Output: Calculation Type: Arithmetic Operations, 30
