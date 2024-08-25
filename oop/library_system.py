27e452c7fdd8a9231787f83e7da4b708caf63db4
ericmuni-23 <munieric44@gmail.com>
2024-08-26 01:18:04 +0300

    A simple script that enhances Additionall and implement a Library class demonstrating composition by managing a collection of books

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 class Book:
-    def __init__(self, title, author):
+    def __str__(self, title, author):
         self.title = title
         self.author = author
 
 class EBook(Book):
-    def __init__(self, title, author, file_size):
-        super().__init__(title, author)
+    def __str__(self, title, author, file_size):
+        super().__str__(title, author)
         self.file_size = file_size
 
 class PrintBook(Book):
-    def __init__(self, title, author, page_count):
-        super().__init__(title, author)
+    def __str__(self, title, author, page_count):
+        super().__str__(title, author)
         self.page_count = page_count
 
 class Library:
-    def __init__(self):
+    def __str__(self):
         self.books = []
 
     def add_book(self, book):
@@ -29,4 +29,3 @@ class Library:
                 print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
             else:
                 print(f"Book: {book.title} by {book.author}")
-                
