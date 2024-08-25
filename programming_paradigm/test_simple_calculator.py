85daaa16cc296055b52a38e33fa1467f98cfef03
ericmuni-23 <munieric44@gmail.com>
2024-08-26 00:42:59 +0300

    A simple script to define and run unit test

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -23,7 +23,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(-1, -1), 1)
         self.assertEqual(self.calc.multiply(0, 10), 0)
     
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(-10, 2), -5)
         self.assertEqual(self.calc.divide(-10, -2), 5)
