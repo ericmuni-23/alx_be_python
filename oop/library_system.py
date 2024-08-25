82b4bd77d8d384875aa81dc090ce89bde0a28a79
ericmuni-23 <munieric44@gmail.com>
2024-08-26 01:15:09 +0300

    A simple script that enhances Additionall and implement a Library class demonstrating composition by managing a collection of books

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,45 +1,32 @@
 #!/usr/bin/env python3
 class Book:
-    def __init__(self, title, author, year):
-        """
-        Constructor to initialize a Book instance.
-        
-        Args:
-        title (str):  Title of the book.
-        author (str): Author of the book.
-        year (int): Publication year of the book.
-        """
+    def __init__(self, title, author):
         self.title = title
         self.author = author
-        self.year = year
 
-    def __del__(self):
-        """
-        Destructor to print a message upon object deletion.
-        """
-        print(f"Deleting {self.title}")
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size
 
-    def __str__(self):
-        """
-        String representation of the Book instance.
-        
-        Returns:
-        str: A string in the format "(title) by (author), published in (year)".
-        """
-        return f"({self.title}) by {self.author}, published in {self.year}"
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
 
-    def __repr__(self):
-        """
-        Official representation of the Book instance.
-        
-        Returns:
-        str: A string that would recreate the Book instance: f"Book('{self.title}', '{self.author}', {self.year})".
-        """
-        return f"Book('{self.title}', '{self.author}', {self.year})"
+class Library:
+    def __init__(self):
+        self.books = []
 
+    def add_book(self, book):
+        self.books.append(book)
 
-# Example usage:
-book = Book("Think big", "Ben Carson", 1994)
-print(book)  # Output: (To Kill a Mockingbird) by Ben Carson, published in 1994
-print(repr(book))  # Output: Book('Think big', 'Ben Carson', 1994)
-del book  # Output: Deleting Think big
+    def list_books(self):
+        for book in self.books:
+            if isinstance(book, EBook):
+                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
+            elif isinstance(book, PrintBook):
+                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
+            else:
+                print(f"Book: {book.title} by {book.author}")
+                
